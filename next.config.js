005eabdbc9d592925f7439f01c23383229608fe1
DeebaUmar1 <deeba.umar@illuminatechnology.com>
2024-11-24 20:15:35 +0500

    config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
-module.exports = {
+const nextConfig = {
     experimental: {
       missingSuspenseWithCSRBailout: false,
     },
-  }
+  };
+  
+  module.exports = nextConfig;
+  
